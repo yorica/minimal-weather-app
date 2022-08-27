68349fe3a221d671f405c4db339b76655af79d3a
yorica <70755276+yorica@users.noreply.github.com>
2022-08-27 19:56:05 +0300

    added weather API and city search

--- scripts/index.js
+++ scripts/index.js
@@ -41,5 +41,37 @@ function showTime() {
   time.innerHTML = `${hour}:${minutes}`;
 }
 
+function changeCity(event) {
+  event.preventDefault();
+  let cityname = document.querySelector("#city");
+  let myCity = cityname.value.toLowerCase();
+  let weUrl = `https://api.openweathermap.org/data/2.5/weather?q=${myCity}&appid=${apiKey}&units=metric`;
+  axios.get(weUrl).then(showTemp);
+}
+
+function showTemp(response) {
+  console.log(response);
+  let h1 = document.querySelector("h1");
+  let myCity = response.data.name;
+  let myCountry = response.data.sys.country;
+  h1.innerHTML = `${myCity}, ${myCountry}`.toUpperCase();
+  let curHum = response.data.main.humidity;
+  let hum = document.querySelector("#hum");
+  hum.innerHTML = `${curHum}%`;
+  let curWind = response.data.wind.speed;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = `${curWind}m/s`;
+  let curTemp = Math.floor(response.data.main.temp);
+  let temp = document.querySelector("#deg");
+  temp.innerHTML = `${curTemp}°C`;
+  let curDescrpt = response.data.weather[0].description;
+  let desc = document.querySelector("#desc");
+  desc.innerHTML = `${curDescrpt}`;
+}
+
+let apiKey = "9ebbcc5f37304a3ed740e8af90680e70";
+let cityForm = document.querySelector("#search-city");
+cityForm.addEventListener("submit", changeCity);
+
 showDate();
 showTime();
