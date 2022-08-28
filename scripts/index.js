4fa6828ca37f5b258c44ee3826580117b22c083f
yorica <70755276+yorica@users.noreply.github.com>
2022-08-28 21:28:31 +0300

    Current location and temperature conversion added

--- scripts/index.js
+++ scripts/index.js
@@ -49,7 +49,6 @@ function changeCity(event) {
 }
 
 function findCity(city) {
-  let apiKey = "9ebbcc5f37304a3ed740e8af90680e70";
   let weUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(weUrl).then(showTemp);
 }
@@ -66,16 +65,53 @@ function showTemp(response) {
   let curWind = response.data.wind.speed;
   let wind = document.querySelector("#wind");
   wind.innerHTML = `${curWind}m/s`;
-  let curTemp = Math.floor(response.data.main.temp);
+  cTemp = Math.floor(response.data.main.temp);
   let temp = document.querySelector("#deg");
-  temp.innerHTML = `${curTemp}°C`;
+  temp.innerHTML = `${cTemp}°C`;
   let desc = document.querySelector("#desc");
   desc.innerHTML = response.data.weather[0].description;
   showDate(response.data.dt * 1000);
   showTime(response.data.dt * 1000);
 }
 
+function goHome(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(findMe);
+}
+
+function findMe(position) {
+  let curLat = position.coords.latitude;
+  let curLon = position.coords.longitude;
+  let weUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${curLat}&lon=${curLon}&appid=${apiKey}&units=metric`;
+  axios.get(weUrl).then(showTemp);
+}
+
+function convertDeg(event) {
+  event.preventDefault();
+  let temp = document.querySelector("#deg");
+  if (temp.className == "deg cel") {
+    temp.className = "deg fah";
+    let fTemp = Math.floor(cTemp * 1.8 + 32);
+    temp.innerHTML = `${fTemp}°F`;
+    btn.innerHTML = `<strong>°C</strong>`;
+  } else {
+    temp.className = "deg cel";
+    temp.innerHTML = `${cTemp}°C`;
+    btn.innerHTML = `<strong>°F</strong>`;
+  }
+}
+
+let apiKey = "9ebbcc5f37304a3ed740e8af90680e70";
+
+let cTemp = null;
+
 let cityForm = document.querySelector("#search-city");
 cityForm.addEventListener("submit", changeCity);
+
+let homeButton = document.querySelector("#home");
+homeButton.addEventListener("click", goHome);
+
+let btn = document.querySelector("#temp");
+btn.addEventListener("click", convertDeg);
 
 findCity("Kyiv");
